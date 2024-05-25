7af643438481bcdef9fde8fb198bb716d56d898c
YDan08 <dasyamashita@gmail.com>
2024-05-25 12:37:44 -0300

    feat: add react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { BrowserRouter } from "react-router-dom"
-import { Router } from "./router"
-import { Main } from "./components/Layout"
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router";
+import { Main } from "./components/Layout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 	return (
@@ -8,8 +10,9 @@ function App() {
 			<BrowserRouter>
 				<Router />
 			</BrowserRouter>
+			<ToastContainer />
 		</Main>
-	)
+	);
 }
 
-export default App
+export default App;
