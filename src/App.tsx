3e368d62acf4ffc0049779c78c1eeec87295f689
YDan08 <dasyamashita@gmail.com>
2024-05-25 18:25:13 -0300

    fix: change name of folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router";
-import { Main } from "./components/Layout";
+import { Main } from "./components/layout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
